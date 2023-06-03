ba21660ac33a70a9591001b584e04da3d233f531
mitsu <s.mitsu0616@gmail.com>
2023-06-03 18:43:50 +0900

    ログイン画面の仮作成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,18 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div class="page">
+    <h1>Login</h1>
+    <div class="login-form">
+      <div class="input_box">
+        <label>メールアドレス</label>
+        <input type="text" name="mail" id="mail" />
+      </div>
+      <div class="btn_box">
+        <button>Login</button>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
