09a53d5846f13df72ed3f03fec516fd3566d39eb
mitsu <s.mitsu0616@gmail.com>
2023-06-04 10:33:01 +0900

    ログアウト実装

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,8 +1,20 @@
-<script setup></script>
+<script setup>
+import { useWorkAppStore } from "../store/workApp";
+import { useRouter } from "vue-router";
+
+const store = useWorkAppStore();
+const router = useRouter();
+
+const logout = () => {
+  store.delUserId();
+  router.push("/login");
+};</script>
 
 <template>
   <div class="page">
     <h1>List</h1>
+    <router-link to="/today">Today</router-link>
+    <button @click="logout">Logout</button>
   </div>
 </template>
 
--- src/views/Today.vue
+++ src/views/Today.vue
@@ -1,8 +1,21 @@
-<script setup></script>
+<script setup>
+import { useWorkAppStore } from "../store/workApp";
+import { useRouter } from "vue-router";
+
+const store = useWorkAppStore();
+const router = useRouter();
+
+const logout = () => {
+  store.delUserId();
+  router.push("/login");
+};
+</script>
 
 <template>
   <div class="page">
     <h1>Today</h1>
+    <router-link to="/list">List</router-link>
+    <button @click="logout">Logout</button>
   </div>
 </template>
 
